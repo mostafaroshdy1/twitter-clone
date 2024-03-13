c6a7f356e91e21520864db32217c238a6b5ec074
Merna Gamal <merna.gamal.shenouda@gmail.com>
2024-03-13 21:19:43 +0200

    Dynamic profile bar

--- src/scripts/home.js
+++ src/scripts/home.js
@@ -29,3 +29,45 @@ function showSearchMessage() {
 function hideSearcMessage() {
     document.getElementById("searchMessage").style.display = "none";
 }
+
+//Dummy Data
+storeUserData("../../public/images/profileImg.jpg","Merna Gamal","Merna.Gamal");
+
+// Function to store user data in local storage
+function storeUserData(imageUrl, name, email) {
+    localStorage.setItem('userImage', imageUrl);
+    localStorage.setItem('userName', name);
+    localStorage.setItem('userEmail', email);
+}
+
+// Function to retrieve user data from local storage and create user object
+function getUserData() {
+    const imageUrl = localStorage.getItem('userImage');
+    const name = localStorage.getItem('userName');
+    const email = localStorage.getItem('userEmail');
+    
+    return {
+        imageUrl: imageUrl,
+        name: name,
+        email: email
+    };
+}
+
+// Function to update profile bar with user data
+function updateProfileBar() {
+    const userData = getUserData();
+    if (userData) {
+        document.querySelector('.profileImage').src = userData.imageUrl;
+        document.getElementById('user_name').innerText = userData.name;
+         // Extracting the username part from the email
+        const emailParts = userData.email.split('@');
+        const username = '@' + emailParts[0];
+        document.getElementById('user_email').innerText = username;
+    }
+}
+
+// Call the updateProfileBar function to fill the data initially
+document.addEventListener("DOMContentLoaded", function() {
+    updateProfileBar();
+});
+
