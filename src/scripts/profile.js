a8a06ece048282f9c9b7878537f90dd8f7f321bc
nadarabea1 <nadarabea81@gmail.com>
2024-03-18 20:24:21 +0200

    Add parts of the edit profile form

--- src/scripts/profile.js
+++ src/scripts/profile.js
@@ -15,3 +15,39 @@ document.addEventListener('DOMContentLoaded', function () {
         });
     });
 });
+
+var modal = document.getElementById('editProfileModal');
+
+var btn = document.getElementById('openModal');
+var span = document.getElementsByClassName('close')[0];
+
+btn.onclick = function () {
+    modal.style.display = 'block';
+}
+
+span.onclick = function () {
+    modal.style.display = 'none';
+}
+
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal.style.display = 'none';
+    }
+}
+
+const inputs = document.querySelectorAll('#editProfileModal input');
+
+inputs.forEach(input => {
+    input.addEventListener('focus', function() {
+        // Add the floating class to the label when input is focused
+        const label = this.nextElementSibling;
+        label.classList.add('floating');
+    });
+    input.addEventListener('blur', function() {
+        // Remove the floating class from the label when input is blurred
+        const label = this.nextElementSibling;
+        if (this.value === '') {
+            label.classList.remove('floating');
+        }
+    });
+});
